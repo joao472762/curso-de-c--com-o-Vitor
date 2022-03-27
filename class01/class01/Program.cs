4ddff543d84c818c59f0590a676c19c49948fdf9
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-03-27 19:28:08 -0300

    write Console.Write('heloword")
    in this class i learning the difernce between the Comand Console.WriteLine() and Console.Wirte()
    the diference is who in Wrilne Line you don't need put  (\n) to broake line and make my fisrt progam in c#

---
+++ class01/class01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("helo word, this is my firtst progam in C#, i'm very exact to do learning " +
+                "this lenguage \n");
+            Console.Write("let's go code");
+
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
