62300b143c10c295183b6af62059205257c130d1
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-03-27 21:53:42 -0300

    //using consnt in my aplication
     //to declare a variable cosnt, just use ( const wishType ConstVariable = 'value)
    
    advise: once deaclare you can't change the variable value, even though it's the same type

---
+++ class04/class04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //use consnt in my aplication
+            //to declare a variable cosnt, just use ( const wishType variableName = 'value)
+
+            const string variableName = "in this, that you deacre a const varialble";
+
+            Console.WriteLine(variableName);
+
+            Console.ReadLine();
+        }
+    }
+}
